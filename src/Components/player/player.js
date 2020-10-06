cb7c14c48867607fa184533453a0b1efe46007f6
shahnshahmalik <shahenshah.malik@hotmail.com>
2020-10-07 00:02:32 +0530

    play pause button modified

--- src/Components/player/player.js
+++ src/Components/player/player.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../style.css'
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
 
-export default function Player() {
+export default function Player(props) {
+  const [playButtonState, setPlayButtonState] = useState(true);
+
   return (
-    <div className={'fl v-cent h-cent start player'}>
-      <SkipPreviousIcon />
+    <div className={'fl v-cent h-cent space player'}>
+      <div className={'player-action-btn'}>
+        <SkipPreviousIcon style={{ fontSize: 40 }}/>
+      </div>
+       {playButtonState && <div onClick={()=> setPlayButtonState(false)} className={'player-action-btn'}>
+       <PlayArrowIcon style={{ fontSize: 70 }}/>
+       </div>}
+       {!playButtonState && <div onClick={()=> setPlayButtonState(true)} className={'player-action-btn'}>
+       <PauseIcon style={{ fontSize: 70 }}/>
+       </div>}
+      <div className={'player-action-btn'}>
+        <SkipNextIcon style={{ fontSize: 40 }}/>
+      </div>
     </div>
   );
 }
